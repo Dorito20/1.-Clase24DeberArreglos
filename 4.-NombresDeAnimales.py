021fc3669fc51f74fb720f46337d7ae323c73bed
Dorito20 <dario.suarez@tecnologicoliceocristiano.edu.ec>
2024-08-10 12:49:49 -0500

    Add files via upload

---
+++ 4.-NombresDeAnimales.py
@@ -0,0 +1,8 @@
+#Crea un arreglo con 5 nombres de animales. Luego, usa un bucle
+
+# Crear un arreglo con los nombres de animales
+nombres = ["gallo", "pez", "toro", "conejo", "águila"]
+
+# Recorrer el arreglo de animales
+for animales in nombres:
+ print("animales:", animales)
